51fa71b4faab2897813e83bb8ed25df487c26e3d
vasketz <anfevach@gmail.com>
2021-09-14 02:58:26 +0000

    fix task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     rsp = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}".format(id))
     rsp2 = requests.get(
-        "https://jsonplaceholder.typicode.com/todos/{}".format(id))
+        "https://jsonplaceholder.typicode.com/users/{}".format(id) + "/todos")
 
     name = rsp.json()["name"]
 
