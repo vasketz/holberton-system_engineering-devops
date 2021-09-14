478bd20987ec466a82e183dff386c8cbc6a9e9b3
vasketz <anfevach@gmail.com>
2021-09-14 02:28:13 +0000

    fix task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -20,16 +20,18 @@ if __name__ == "__main__":
 
     done_task = 0
     total_task = 0
-    title_list = []
+    title_list = ""
 
     for task in rsp2.json():
         if task.get("userId") == int(id):
             if task.get('completed'):
                 done_task += 1
-                title_list.append("\t " + task.get("title"))
+                title_list = title_list + "\t" + task.get("title") + "\n"
+                # title_list.append("\t " + task.get("title"))
             total_task += 1
 
     print("Employee {} is done with task({}/{}):".format(name,
                                                          done_task,
                                                          total_task))
-    print('\n'.join(title_list))
+    title_list = title_list.rstrip(title_list[-1])
+    print(title_list)
