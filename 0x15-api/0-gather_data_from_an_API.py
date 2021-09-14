a5a55d5a8177775c749d36ab3f7834b3bcf1cc47
vasketz <anfevach@gmail.com>
2021-09-14 02:04:10 +0000

    Update task 0 and readme

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This script return information about his/her todo list
+"""
+
+
+import requests
+from sys import argv
+
+
+id = argv[1]
+
+rsp = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(id))
+rsp2 = requests.get("https://jsonplaceholder.typicode.com/todos")
+
+name = rsp.json()["name"]
+
+done_task = 0
+total_task = 0
+title_list = []
+
+for task in rsp2.json():
+    if task.get("userId") == int(id):
+        if task.get('completed'):
+            done_task += 1
+            title_list.append("\t " + task.get("title"))
+        total_task += 1
+
+print("Employee {} is done with task({}/{}):".format(name,
+                                                     done_task,
+                                                     total_task))
+print('\n'.join(title_list))
