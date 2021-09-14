4c288fd5dca630d09c13b3d4f7bf62fa2abd3959
vasketz <anfevach@gmail.com>
2021-09-14 02:11:38 +0000

    Fix task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,29 +4,31 @@ This script return information about his/her todo list
 """
 
 
-import requests
 from sys import argv
+import requests
 
+if __name__ == "__main__":
+    id = argv[1]
 
-id = argv[1]
+    rsp = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(id))
+    rsp2 = requests.get(
+        "https://jsonplaceholder.typicode.com/todos")
 
-rsp = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(id))
-rsp2 = requests.get("https://jsonplaceholder.typicode.com/todos")
+    name = rsp.json()["name"]
 
-name = rsp.json()["name"]
+    done_task = 0
+    total_task = 0
+    title_list = []
 
-done_task = 0
-total_task = 0
-title_list = []
+    for task in rsp2.json():
+        if task.get("userId") == int(id):
+            if task.get('completed'):
+                done_task += 1
+                title_list.append("\t " + task.get("title"))
+            total_task += 1
 
-for task in rsp2.json():
-    if task.get("userId") == int(id):
-        if task.get('completed'):
-            done_task += 1
-            title_list.append("\t " + task.get("title"))
-        total_task += 1
-
-print("Employee {} is done with task({}/{}):".format(name,
-                                                     done_task,
-                                                     total_task))
-print('\n'.join(title_list))
+    print("Employee {} is done with task({}/{}):".format(name,
+                                                         done_task,
+                                                         total_task))
+    print('\n'.join(title_list))
