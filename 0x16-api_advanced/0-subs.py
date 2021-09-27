3a34c604881aee4f27a000d6b3ac1ad615ae7d7e
vasketz <anfevach@gmail.com>
2021-09-27 22:43:39 +0000

    Update readme and task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-
+"""
+Write a function that queries the Reddit API
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """ returns the number of subscribers """
     url = "https://www.reddit.com/r/" + subreddit + "/about.json"
     rsp = requests.get(url, headers={'user-agent': 'vasketz'},
                        allow_redirects=False)
@@ -13,6 +16,3 @@ def number_of_subscribers(subreddit):
         return data
     else:
         return 0
-        
-
-            
